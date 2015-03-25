b9900731659c2bc5f4f2828b654cc79d10bd4b27
cdoli <kalidkc@gmail.com>
2015-03-25 00:19:45 -0400

    REST API example

---
+++ src/main/java/com/temp/conversion/TempratureConversionService.java
@@ -0,0 +1,8 @@
+package com.temp.conversion;
+
+import javax.ws.rs.Path;
+
+@Path("/convert")
+public class TempratureConversionService {
+
+}
---
+++ src/main/java/com/vending/rest/VendingMachineService.java
@@ -0,0 +1,9 @@
+package com.vending.rest;
+
+import javax.ws.rs.Path;
+
+@Path("/hello")
+public class VendingMachineService {
+
+	
+}
